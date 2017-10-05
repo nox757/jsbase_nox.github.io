5d57d0b8badc9c260d2ac30331bb4edfae366a81
nox757 <nox757@gmail.com>
2017-10-05 17:16:39 +0300

    Add founded demonstrate

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,41 @@
-var myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+// document.querySelector('html').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+//change images on click
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === './images/img_cat.jpg') {
+        // alert("OHH");
+      myImage.setAttribute ('src','./images/img_cat2.jpg');
+    } else {
+      myImage.setAttribute ('src','./images/img_cat.jpg');
+    }
+}
+
+//add personal greeting
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML  = 'Cat is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML  = 'Cat is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+
